ffd907ede2ad7b4de69739e49619fe75c035e9f7
fredrikb12 <fredrik.backman12@gmail.com>
2022-03-31 01:07:47 +0200

    Clean up unused code

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -11,7 +11,6 @@ function RouteSwitch() {
         <Route path="/" element={<Navbar />}>
           <Route index element={<Homepage />} />
           <Route path="/shop" element={<Shop />}>
-            <Route index element={<div>Index element</div>} />
             <Route path="/shop/:pokemonId" element={<Pokemon />} />
             <Route path="/shop/checkout" element={<div></div>} />
           </Route>
