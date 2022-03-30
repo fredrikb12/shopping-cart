c8483dc0f719cade8ba3eeea34f317501f7dd700
fredrikb12 <fredrik.backman12@gmail.com>
2022-03-30 22:33:42 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./components/App";
 import RouteSwitch from "./components/RouteSwitch";
 
 const container = document.getElementById("root");
