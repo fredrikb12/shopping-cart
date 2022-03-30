265e90a94f01dbcc9bcc641ecb6f1ea9535a0712
fredrikb12 <fredrik.backman12@gmail.com>
2022-03-30 22:34:45 +0200

    Implement specific Pokemon view

---
+++ src/components/Pokemon.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useOutletContext, useParams } from "react-router-dom";
+import { getPokemonArray } from "../data/pokemonData";
+import PokemonCard from "./PokemonCard";
+
+function Pokemon({}) {
+  let params = useParams();
+  const onClick = useOutletContext();
+  const pokemonArray = getPokemonArray();
+  const pokemon = pokemonArray.find((el) => {
+    return el.num === parseInt(params.pokemonId);
+  });
+  const willRenderPokemon = pokemon === undefined ? false : true;
+  if (willRenderPokemon) {
+    return (
+      <div>
+        <h1>This is the Pokemon!</h1>
+        <PokemonCard name={pokemon.name} isShop={true} onClick={onClick} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="error-container">
+        <h1 className="error-message">
+          Pokemon with index: {params.pokemonId} doesn't exist in our database!
+          Please try again.
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Pokemon;
